ff242a1f8df8e9dd418d2207649986f03e3bac86
Minilay <minilie88@gmail.com>
2022-08-06 17:51:58 +0600

    Planet-Alpha

---
+++ components/Planet.tsx
@@ -0,0 +1,21 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model from './Planetdemo'
+
+export default function Planet() :JSX.Element {
+  return (
+    <Canvas
+         camera={{ position: [2, 0, 12.25], fov: 90 }}
+         style={{
+            width: '500px',
+            height: '500px',
+         }}
+      >
+         <ambientLight intensity={1.25} />
+         <ambientLight intensity={0.1} />
+         <directionalLight intensity={0.4} />
+          <Model/>
+         <OrbitControls />
+      </Canvas>
+  )
+}
--- components/hero.tsx
+++ components/hero.tsx
@@ -3,6 +3,7 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import SolidButton from './solidButton'
 
+import Planet from './Planet'
 
 const navigation = [
   { name: 'Registration', href: '#' },
@@ -146,12 +147,16 @@ export default function HeroSection() {
         Image block
         TODO: place a 3d planet rotating
       */}
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      {/* <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
+      </div> */}
+
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/3">
+        <Planet/>
       </div>
     </div>
   )
