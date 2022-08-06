2555694e458e12063a2e7e78684ab3fbd3748f8a
Aldiyar Serikov <44750760+blusrc@users.noreply.github.com>
2022-08-06 19:22:51 +0600

    hero hotfix

--- components/hero.tsx
+++ components/hero.tsx
@@ -143,29 +143,9 @@ export default function HeroSection() {
           </main>
         </div>
       </div>
-      {/* 
-        Image block
-        TODO: place a 3d planet rotating
-      */}
-<<<<<<< HEAD
-      {/* <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt=""
-        />
-      </div> */}
-
+      
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/3">
         <Planet/>
-=======
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        {/* <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt=""
-        /> */}
->>>>>>> footer
       </div>
     </div>
   )
