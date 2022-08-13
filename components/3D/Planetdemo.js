fd11c29508bf3e034fd4f1bbc9c782df448b6a1b
Minilay <minilie88@gmail.com>
2022-08-13 15:03:10 +0600

    trash commit

--- components/3D/Planetdemo.js
+++ components/3D/Planetdemo.js
@@ -10,7 +10,7 @@ import * as THREE from "three"
 
 //TODO: Change URL for live-url on production. 
 // const URL = "http://localhost:3000/models/planet.glb";
-const URL = "https://tubular-sunburst-06da0f.netlify.app";
+const URL = "https://tubular-sunburst-06da0f.netlify.app/models/planet.glb";
 
 
 export default function PlanetModel(props) {
