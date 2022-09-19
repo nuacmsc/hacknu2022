d0554ecc8e8ca7ad9bbbefa5861276b90d6d3435
Aldiyar <ldr.stlt@gmail.com>
2022-09-19 10:14:25 +0600

    tassay logo

--- components/partners.tsx
+++ components/partners.tsx
@@ -1,12 +1,13 @@
 export default function () {
     return (
-        <div id="partners" className="flex flex-col items-center py-8">
+        <div id="partners" className="flex flex-col items-center py-8 px-8 md-px-16">
             <h1 className="font-bold text-3xl mb-8 text-gray-800">Our sponsors</h1>
             <div className="flex flex-row flex-wrap gap-4">
                 <img className="w-20 md:w-32 lg:w-48 mr-4" src="google.svg" alt="Google" />
                 <img className="w-20 md:w-32 lg:w-48" src="epam.svg" alt="EPAM" />
                 <img className="w-20 md:w-32 lg:w-48" src="beeline.svg" alt="Beeline" />
                 <img className="w-20 md:w-32 lg:w-48" src="nuris.svg" alt="NURIS" />
+                <img className="w-20 md:w-32 lg:w-48" src="logo_tassay.svg" alt="Tassay" />
             </div>
         </div>
     )
